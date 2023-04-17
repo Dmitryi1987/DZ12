5e523b46e8623e3d6c578e2da11057a7176e3598
Dmitry1987 <vasyutinskii5@gmail.com>
2023-04-17 21:17:48 +0300

    Changes my project

--- src/main/java/ru/netology/PosterManager.java
+++ src/main/java/ru/netology/PosterManager.java
@@ -1,65 +1,39 @@
 package ru.netology;
 
 public class PosterManager {
-    private String[] movie = { "movie1", "movie2", "movie3", "movie4", "movie4",  "movie5", "movie6", "movie7"
+    private int movie;
 
-    };
-
-    public PosterManager(String film) {
-
-        this.movie = movie;
-    }
-
-    public PosterManager() {
-    }
-    public PosterManager(int size) {
-
-    }
-
-    public String[] getMovie() {
-        return movie;
-    }
-
-    public void setMovie(String[] movie) {
-
+    public PosterManager(int movie) {
         this.movie = movie;
     }
 
     private PosterManager[] movies = new PosterManager[0];
 
     public void add(PosterManager movie) {
-        PosterManager[] tmp = new PosterManager[movies.length+1];
-        for (int i=0; i < movies.length; i++) {
+        PosterManager[] tmp = new PosterManager[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
             tmp[i] = movies[i];
         }
         tmp[tmp.length - 1] = movie;
         movies = tmp;
     }
 
-    public void  findAll(PosterManager movie) {
-        PosterManager[] tmp = new PosterManager[movies.length + 1];
-        for (int i=0; i < movies.length; i++) {
-            tmp[i] = movies[i];
+    public PosterManager[] findAll() {
+        return movies;
+    }
+
+    public PosterManager[] findLast() {
+        int resultLength = movies.length;
+        if (resultLength >= movie) {
+            resultLength = movie;
+        } else {
+            resultLength = movies.length;
         }
-        tmp[tmp.length -1] = movie;
-        movies = tmp;
-
-    }
-
-
-    public void findLast(PosterManager) {
-
-    }
-        ??? resultLength;
-  if ??? {
-        resultLength = ???
-    } else {
-        resultLength = ???
-    }
-  ??? result = new ???[resultLength];
-  for (???) {
-        // заполняем result из массива, что лежит в поле
-        // в обратном порядке
+        PosterManager[] result = new PosterManager[resultLength];
+        for (int i = 0; i < resultLength; i++) {
+            result[i] = movies[resultLength - 1 - i];
+        }
+        return result;
     }
 }
 
